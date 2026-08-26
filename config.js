1f6e4d22172bba2bbdf619ecb1301f5cb7aeb2c9
okinamap <kenkn2023@gmail.com>
2026-08-26 16:04:22 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-window.OKINAMAP_CONFIG={mapTilerKey:"aX2MxvPkZmH5QUQM4PAP",initialCenter:[127.926,26.456],initialZoom:13,maxBounds:[[127.48,25.95],[128.42,27.10]],minZoom:8.2,maxZoom:18,mapStyle:"https://api.maptiler.com/maps/streets-v2/style.json?key={KEY}"};
+window.OKINAMAP_CONFIG = {
+  mapTilerKey: "aX2MxvPkZmH5QUQM4PAP",
+  initialCenter: [127.926, 26.456],
+  initialZoom: 13,
+  maxBounds: [[127.48, 25.95], [128.42, 27.10]],
+  minZoom: 8.2,
+  maxZoom: 18,
+  mapStyle: "https://api.maptiler.com/maps/streets-v2/style.json?key={KEY}"
+};
