b96db1c4da961037291e46fdb22e1acce948da6b
okinamap <kenkn2023@gmail.com>
2026-08-26 17:43:24 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.OKINAMAP_CONFIG={
   mapTilerKey:"aX2MxvPkZmH5QUQM4PAP",
   initialCenter:[127.926,26.456],initialZoom:13,
   maxBounds:[[127.48,25.95],[128.42,27.10]],minZoom:8.2,maxZoom:18,
-  mapStyle:"https://api.maptiler.com/maps/streets-v2/style.json?key={KEY}"
+  mapStyle:"https://api.maptiler.com/maps/01a03d38-fd9e-76f2-bc3b-24b1aef41074/style.json?key=aX2MxvPkZmH5QUQM4PAP"
 };
