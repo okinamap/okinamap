b6900944fe30490c2adc1caf0bbd1b3a0509e11f
okinamap <kenkn2023@gmail.com>
2026-08-27 12:51:26 +0900

    Update spots.js

--- data/spots.js
+++ data/spots.js
@@ -424,10 +424,6 @@ window.OKINAMAP_SPOTS=[
         {
         src: "assets/spots/kin/other-002-02.webp",
         position: "center"
-        },
-        {
-        src: "assets/spots/kin/other-002-03.webp",
-        position: "center"
         }
     ],
     groupId: null
